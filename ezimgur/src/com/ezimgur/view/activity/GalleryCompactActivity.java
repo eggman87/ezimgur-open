db69841514ce2824858c66e808c2e0e59556d8da
Matthew Harris <maharris.us@gmail.com>
2013-12-25 21:13:18 -0700

    adding old features.

--- ezimgur/src/com/ezimgur/view/activity/GalleryCompactActivity.java
+++ ezimgur/src/com/ezimgur/view/activity/GalleryCompactActivity.java
@@ -4,19 +4,30 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.view.ViewPager;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 import com.ezimgur.R;
 import com.ezimgur.control.IGalleryController;
+import com.ezimgur.data.GalleryManager;
 import com.ezimgur.datacontract.GalleryItem;
 import com.ezimgur.datacontract.GalleryItemComposite;
 import com.ezimgur.datacontract.GallerySort;
 import com.ezimgur.instrumentation.Log;
+import com.ezimgur.persistance.datasource.GalleryDataSource;
 import com.ezimgur.task.InitApplicationTask;
+import com.ezimgur.task.SearchGalleryTask;
 import com.ezimgur.view.adapter.CaptionAdapter;
 import com.ezimgur.view.adapter.ImagesViewPagerAdapter;
-import com.ezimgur.view.event.GalleryCaptionLoadEvent;
-import com.ezimgur.view.event.GalleryLoadEvent;
-import com.ezimgur.view.event.InitApplicationEvent;
+import com.ezimgur.view.event.*;
+import com.ezimgur.view.fragment.DialogChangeDays;
+import com.ezimgur.view.fragment.DialogChangeGallery;
+import com.ezimgur.view.fragment.DialogGalleryItemDetail;
 import com.ezimgur.view.fragment.ItemDetailsFragment;
 import roboguice.event.Observes;
 import roboguice.inject.InjectFragment;
@@ -32,17 +43,22 @@ import java.util.List;
  * Date: 12/25/13
  * Time: 12:32 PM
  */
-public class GalleryCompactActivity extends BaseActivity {
+public class GalleryCompactActivity extends BaseActivity implements DialogChangeGallery.OnChangeGalleryListener {
 
     @Inject IGalleryController controller;
     @InjectView(R.id.screen_gallery_compact_vp_images) ViewPager viewPager;
     @InjectView(R.id.screen_gallery_compact_lv_captions) ListView listCaptions;
+    @InjectView(R.id.banana_for_scale)ImageView imgBananaForScale;
     @InjectFragment(R.id.screen_gallery_compact_frag_item_details)ItemDetailsFragment detailsFragment;
 
     private String currentGallery;
     private List<GalleryItemComposite> composites;
+    private GallerySort currentSort;
     private int currentPosition;
     private int currentDays;
+    private boolean isSearchGallery;
+    private boolean isBananaListenerSet;
+    private boolean saveSubReddit;
 
     private static final String BUNDLE_STATE_GALLERY = "bundle_state_gallery";
     private static final String BUNDLE_STATE_CURRENT_DAYS = "bundle_state_daysago";
@@ -109,6 +125,102 @@ public class GalleryCompactActivity extends BaseActivity {
         detailsFragment.setGalleryItem(composites.get(currentPosition).galleryItem);
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+
+        menu.add(MENU_REFRESH)
+                .setIcon(android.R.drawable.ic_popup_sync);
+        menu.add(CONTEXT_MENU_COMMENT_ITEM)
+                .setIcon(android.R.drawable.ic_menu_edit)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        menu.add(MENU_CHANGE_GALLERY)
+                .setIcon(android.R.drawable.ic_menu_gallery)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        menu.add(MENU_CHANGE_DAYSAGO)
+                .setIcon(android.R.drawable.ic_menu_today)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        menu.add(MENU_BANANA_FOR_SCALE)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
+        menu.add(MENU_RELOAD_COMMENTS)
+                .setIcon(android.R.drawable.ic_popup_sync);
+        com.actionbarsherlock.widget.SearchView searchView = new com.actionbarsherlock.widget.SearchView(getSupportActionBar().getThemedContext());
+        final MenuItem searchItem = menu.add(MENU_SEARCH_GALLERY);
+        searchItem.setIcon(android.R.drawable.ic_menu_search)
+                .setActionView(searchView)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM | MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW);
+
+        searchView.setQueryHint("search gallery…");
+        searchView.setOnQueryTextListener(new com.actionbarsherlock.widget.SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(final String query) {
+                Toast.makeText(getApplicationContext(), "searching for " + query, Toast.LENGTH_LONG).show();
+                new SearchGalleryTask(getApplicationContext(), query){
+
+                    @Override
+                    protected void onSuccess(List<GalleryItem> galleryItems) throws Exception {
+                        super.onSuccess(galleryItems);
+
+                        if (galleryItems != null && galleryItems.size() > 0){
+                            Toast.makeText(getContext(), galleryItems.size() + " images found!", Toast.LENGTH_LONG).show();
+                            searchItem.collapseActionView();
+
+                            setItemsToComposites(galleryItems);
+                            currentGallery = "search - " + query;
+                            getSupportActionBar().setTitle(currentGallery);
+                            isSearchGallery = true;
+                            onCompositesReady();
+
+                            if (saveSubReddit ) {
+                                GalleryDataSource dataSource = GalleryDataSource.newInstance(getContext());
+                                dataSource.createSavedGallery(currentGallery);
+                                saveSubReddit = false;
+                            }
+
+                        } else {
+                            Toast.makeText(getContext(), "No images found for search. Please try other terms.", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }.execute();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        CharSequence title = item.getTitle();
+        if (title == MENU_REFRESH)  {
+            if (isSearchGallery)
+                Toast.makeText(this, "you can not refresh a search, change gallery or research", Toast.LENGTH_SHORT).show();
+            else
+               controller.loadGallery(currentGallery == null ? "hot" : currentGallery, currentDays, currentSort);
+        } else if (title == MENU_CHANGE_GALLERY) {
+            changeGallery();
+        } else if (title == MENU_CHANGE_DAYSAGO) {
+            changeDaysAgo();
+        } else if (title == CONTEXT_MENU_COMMENT_ITEM) {
+            openItemDetailFragment();
+        } else if (title == MENU_RELOAD_COMMENTS) {
+            controller.loadCaptions(composites.get(currentPosition), currentPosition);
+        } else if (title == MENU_SEARCH_GALLERY) {
+            if (!item.isActionViewExpanded())
+                item.expandActionView();
+        } else if (title == MENU_BANANA_FOR_SCALE){
+            toggleBananaForScale();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     public void onApplicationInitialized(@Observes InitApplicationEvent event){
         Log.d(TAG, "application initialized - loading gallery for first time");
 
@@ -116,26 +228,27 @@ public class GalleryCompactActivity extends BaseActivity {
         String galleryName = pref.getString("default_gallery", "hot");
         GallerySort sort = GallerySort.values()[pref.getInt("default_sort", 0)];
 
+        currentSort = sort;
+        currentDays = 0;
 
         controller.loadGallery(galleryName, 0, sort);
         isRefreshingToken = false;
     }
 
-
     public void onGalleryeLoad(@Observes GalleryLoadEvent event) {
         currentGallery = event.galleryName;
         getSupportActionBar().setTitle(currentGallery);
+        setItemsToComposites(event.galleryItems);
+        onCompositesReady();
+    }
 
-
-        List<GalleryItem> loadedItems = event.galleryItems;
-        composites = new ArrayList<GalleryItemComposite>(loadedItems.size());
-        for (GalleryItem item : loadedItems) {
+    private void setItemsToComposites(List<GalleryItem> items) {
+        composites = new ArrayList<GalleryItemComposite>(items.size());
+        for (GalleryItem item : items) {
             GalleryItemComposite composite = new GalleryItemComposite();
             composite.galleryItem = item;
             composites.add(composite);
         }
-
-        onCompositesReady();
     }
 
     public void onCaptionsLoaded(@Observes GalleryCaptionLoadEvent event) {
@@ -173,5 +286,74 @@ public class GalleryCompactActivity extends BaseActivity {
             }
         });
 
+    }
+
+    private void openItemDetailFragment() {
+        if (composites == null)
+            return;
+        DialogGalleryItemDetail dialogFragment = DialogGalleryItemDetail.newInstance(composites.get(currentPosition).galleryItem, currentGalleryHasComments());
+        dialogFragment.show(getSupportFragmentManager(), DialogGalleryItemDetail.TAG);
+    }
+
+    private boolean currentGalleryHasComments() {
+        return GalleryManager.galleryHasComments(currentGallery);
+    }
+
+    private void changeDaysAgo() {
+        DialogChangeDays dialogFragment = DialogChangeDays.newInstance(currentDays, currentSort);
+        dialogFragment.show(getSupportFragmentManager(), TAG);
+    }
+
+    private void changeGallery() {
+        DialogChangeGallery changeDialog = DialogChangeGallery.newInstance(
+                currentGallery == null ? "hot" : currentGallery, currentSort);
+        changeDialog.show(getSupportFragmentManager(), TAG);
+    }
+
+    private void toggleBananaForScale(){
+
+        imgBananaForScale.setVisibility(imgBananaForScale.getVisibility() == View.GONE ? View.VISIBLE:View.GONE);
+        imgBananaForScale.bringToFront();
+        if (!isBananaListenerSet) {
+            imgBananaForScale.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+
+                    int eid = event.getAction();
+                    switch (eid) {
+                        case MotionEvent.ACTION_MOVE:
+
+                            RelativeLayout.LayoutParams mParams = (RelativeLayout.LayoutParams) imgBananaForScale.getLayoutParams();
+                            int x = (int) event.getRawX();
+                            int y = (int) event.getRawY();
+                            mParams.leftMargin = x-20;
+                            mParams.topMargin = y-20;
+                            imgBananaForScale.setLayoutParams(mParams);
+
+
+                            break;
+
+                        default:
+                            break;
+                    }
+                    return true;
+                }
+            });
+            isBananaListenerSet = true;
+        }
+    }
+
+    @Override
+    public void changeGalleryClicked(String galleryName, boolean saveSubReddit, GallerySort sort, boolean makeDefault) {
+        this.saveSubReddit = saveSubReddit;
+        controller.loadGallery(galleryName, 0, sort, true, makeDefault);
+    }
+
+    public void onDaysAgoSelectionChanged(@Observes ChangeDaysAgoEvent event) {
+        controller.loadGallery(currentGallery == null ? "hot":currentGallery, event.getDaysAgo(), currentSort, true, false);
+    }
+
+    public void onCommentSubmitted(@Observes CommentSubmittedEvent event) {
+        controller.loadCaptions(composites.get(currentPosition), currentPosition);
     }
 }
